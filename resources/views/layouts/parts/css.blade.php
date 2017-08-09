cf07268a3855027cd7ee5f738996a89782a1f1c0
James Harper <jameswilliamharper@gmail.com>
2017-08-09 22:26:59 +0100

    Let laravel mix handle minification

--- resources/views/layouts/parts/css.blade.php
+++ resources/views/layouts/parts/css.blade.php
@@ -1,3 +1,3 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.1.2/css/bulma.min.css" />
 <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet">
-<link rel="stylesheet" href="./css/app.min.css" />
+<link rel="stylesheet" href="./css/app.css" />
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,7 +1,4 @@
 let mix = require('laravel-mix');
-let minifier = require('minifier');
 
 mix.js('resources/assets/js/app.js', 'public/js');
-
 mix.styles(['resources/assets/css/*.css'], 'public/css/app.css');
-minifier.minify('public/css/app.css');
