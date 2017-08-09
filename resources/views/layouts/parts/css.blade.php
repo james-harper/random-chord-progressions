c3d9e85a62d89606143c67a0c3a577823bd33178
James Harper <jameswilliamharper@gmail.com>
2017-08-09 01:09:11 +0100

    Move css

--- resources/views/layouts/parts/css.blade.php
+++ resources/views/layouts/parts/css.blade.php
@@ -1,16 +1,3 @@
- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.1.2/css/bulma.min.css" /> 
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.1.2/css/bulma.min.css" />
 <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-T8Gy5hrqNKT+hzMclPo118YTQO6cYprQmhrYwIiQ/3axmI1hQomh7Ud2hPOy8SP1" crossorigin="anonymous">
-<style>
-html, body
-{
-    width: 100%;
-    height: 100%;
-    margin: 0;
-    padding: 0;
-    background: #FFF
-}
-
-.is-centered {
-    text-align:center;
-}
-</style>
+<link rel="stylesheet" href="./css/app.css" />
