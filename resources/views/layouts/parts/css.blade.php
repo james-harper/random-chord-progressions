ba5a19965e7b59c816e5ba10063429193f82ba00
James Harper <jameswilliamharper@gmail.com>
2017-08-09 20:25:25 +0100

    Simplify link tag

--- resources/views/layouts/parts/css.blade.php
+++ resources/views/layouts/parts/css.blade.php
@@ -1,3 +1,3 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.1.2/css/bulma.min.css" />
-<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-T8Gy5hrqNKT+hzMclPo118YTQO6cYprQmhrYwIiQ/3axmI1hQomh7Ud2hPOy8SP1" crossorigin="anonymous">
+<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet">
 <link rel="stylesheet" href="./css/app.css" />
