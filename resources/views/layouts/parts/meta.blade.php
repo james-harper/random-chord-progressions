79a16675c717ecf826f06d80667f1cdbdd572d57
James Harper <jameswilliamharper@gmail.com>
2017-08-09 20:46:49 +0100

    Added meta tags

--- resources/views/layouts/parts/meta.blade.php
+++ resources/views/layouts/parts/meta.blade.php
@@ -1,2 +1,5 @@
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="description" content="Random Chord Progression Generator">
+<meta name="keywords" content="Music, Chord Progression, Randomly generated, Inspiration, guitar, tablature">
+<meta name="author" content="James Harper">
