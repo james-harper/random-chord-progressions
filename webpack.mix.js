eb50ad9c72d997e188789360a50ddce4a0d81039
James Harper <jameswilliamharper@gmail.com>
2017-08-09 22:15:01 +0100

    Update mix file to handle css

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,14 +1,7 @@
 let mix = require('laravel-mix');
-
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for the application as well as bundling up all the JS files.
- |
- */
+let minifier = require('minifier');
 
 mix.js('resources/assets/js/app.js', 'public/js');
+
+mix.styles(['resources/assets/css/app.css'], 'public/css/app.css');
+minifier.minify('public/css/app.css');
