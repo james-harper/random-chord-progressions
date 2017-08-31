410f69a27e8ee38b908d5521f67784c57849309c
James Harper <jameswilliamharper@gmail.com>
2017-08-31 22:44:27 +0100

    Fix indentatio

--- webpack.mix.js
+++ webpack.mix.js
@@ -5,9 +5,9 @@ mix.styles(['resources/assets/css/*.css'], 'public/css/app.css').version();
 mix.sass('resources/assets/css/bulma.scss', 'public/css/');
 
 mix.webpackConfig({
-    resolve: {
-      alias: {
-        '@': path.resolve('resources/assets/js')
-      }
+  resolve: {
+    alias: {
+      '@': path.resolve('resources/assets/js')
     }
-  });
+  }
+});
