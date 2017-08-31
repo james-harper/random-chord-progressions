c77200459bed36fd9106f5000afc8974fb8f13ad
James Harper <jameswilliamharper@gmail.com>
2017-08-31 22:45:43 +0100

    Change order of methods

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,8 +1,8 @@
 let mix = require('laravel-mix');
 
 mix.js('resources/assets/js/app.js', 'public/js').version();
-mix.styles(['resources/assets/css/*.css'], 'public/css/app.css').version();
 mix.sass('resources/assets/css/bulma.scss', 'public/css/');
+mix.styles(['resources/assets/css/*.css'], 'public/css/app.css').version();
 
 mix.webpackConfig({
   resolve: {
